852dfb2fde71b8461e6ac712ed6d798eb59d4b95
kimdoomin <kimduumin@gmail.com>
2022-07-17 13:54:27 +0900

    mongo db 적용 중

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,18 @@
-const authModel = require("../models/wordModel")
+const authModel = require("../models/authModel")
 const axios = require("axios")
 const cheerio = require("cheerio")
 const Iconv = require("iconv-lite")
+const jwt = require('jsonwebtoken')
 
 const login = async function(req, res) {
     try{
-        res.json("1")
+        const secretKey = process.env.SECRET_KEY
+        const algorithm = process.env.JWT_ALG
+        const expiresIn = process.env.JWT_EXP
+        const issuer = process.env.JWT_ISSUER
+        const option = {algorithm, expiresIn, issuer}
+        const result = jwt.sign(payload, secretKey, option)
+        res.json(result)
     } catch (e){
         res.json(e)
     }
